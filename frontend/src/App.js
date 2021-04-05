6c43028bf3252fe27ebc75ea27c373cdc84a201e
Stipo Margić <stipo@liberato.io>
2021-04-05 12:50:39 +0200

    Added hero section to homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./Components/Header";
+import HomepageHero from "./Components/HomepageHero";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <HomepageHero />
     </BrowserRouter>
   );
 }
