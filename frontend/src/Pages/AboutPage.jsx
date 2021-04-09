e117429209ff65cf6fc6f176d211abd420093bde
Stipo Margic <stipo@liberato.io>
2021-04-09 21:09:02 +0200

    Added numbers in about section

--- frontend/src/Pages/AboutPage.jsx
+++ frontend/src/Pages/AboutPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AboutUs from "../Components/AboutUs";
 import WhatPeopleSaying from "../Components/Sliders/WhatPeopleSayingSlider";
+import AboutNumbers from "../Components/AboutUs/AboutNumbers";
 
 const AboutPage = () => {
   return (
     <>
       <AboutUs />
+      <AboutNumbers />
       <WhatPeopleSaying />
     </>
   );
