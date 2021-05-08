3d5b8997d04edcf2727b2fd530109e888ed93801
Stipo Margic <stipo@liberato.io>
2021-05-08 17:21:42 +0200

    Category Query and List Controller

---
+++ backend/src/Infrastructure/Service/QueryHandler/Web/Category/CategoryQueryHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Infrastructure\Service\QueryHandler\Web\Category;
+
+
+use App\Application\Query\Category\CategoryQuery;
+use App\Domain\Category\Category;
+use Doctrine\ORM\EntityManagerInterface;
+use Undabot\JsonApi\Implementation\Model\Request\Sort\SortSet;
+use Undabot\SymfonyJsonApi\Model\Collection\ArrayCollection;
+use Undabot\SymfonyJsonApi\Service\Pagination\Paginator;
+
+class CategoryQueryHandler
+{
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
+
+    public function __invoke(CategoryQuery $query): \Undabot\SymfonyJsonApi\Model\Collection\ObjectCollection|ArrayCollection
+    {
+        $qb = $this->entityManager->createQueryBuilder()->select('c')->from(Category::class, 'c');
+
+        if (property_exists($query, 'sortSet') && null !== $query->sortSet && $query->sortSet instanceof SortSet) {
+            foreach ($query->sortSet->getSortsArray() as $column => $direction) {
+                $qb = $qb->orderBy('c.' . $column, $direction);
+            }
+        }
+
+        return (null !== $query->offset && null !== $query->size)
+            ? (new Paginator())
+                ->createPaginatedCollection(
+                    $qb,
+                    $query->offset,
+                    $query->size
+                )
+            : new ArrayCollection($qb->getQuery()->getResult());
+    }
+}
--- backend/src/Infrastructure/UI/HTTP/Web/v1/Endpoint/Category/ListCategoryController.php
+++ backend/src/Infrastructure/UI/HTTP/Web/v1/Endpoint/Category/ListCategoryController.php
@@ -4,4 +4,29 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\UI\HTTP\Web\v1\Endpoint\Category;
 
-final class ListCategoryController {}
+use App\Application\Query\Category\CategoryQuery;
+use App\Application\Service\Bus\QueryBus;
+use App\Infrastructure\UI\HTTP\Web\v1\ApiResponder\ResourceResponder;
+use Symfony\Component\Routing\Annotation\Route;
+use Undabot\JsonApi\Definition\Model\Request\GetResourceCollectionRequestInterface;
+use Undabot\SymfonyJsonApi\Http\Model\Response\ResourceCollectionResponse;
+
+final class ListCategoryController
+{
+    #[Route('/api/v1/categories', name: 'api_v1_categories_list', methods: ['GET'])]
+    public function list(
+        GetResourceCollectionRequestInterface $request,
+        QueryBus $queryBus,
+        ResourceResponder $responder
+    ): ResourceCollectionResponse {
+        $request->allowSorting(['name', 'createdAt']);
+
+        $pagination = $request->getPagination();
+
+        $entities = $queryBus->handleQuery(new CategoryQuery(null === $pagination ? null : $pagination->getOffset(),
+            null ===
+            $pagination ? null : $pagination->getSize(), $request->getSortSet()));
+
+        return $responder->resourceObjectCollection($entities);
+    }
+}
