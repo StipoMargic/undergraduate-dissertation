ab7976aa5ff4dc81e41b0febe1db9e6c7bf350f6
Stipo Margic <stipo@liberato.io>
2021-07-07 14:38:27 +0200

    Sending email when someone wants to hire you

--- backend/src/Application/Service/CommandHandler/Web/Portfolio/HireFreelancerCommandHandler.php
+++ backend/src/Application/Service/CommandHandler/Web/Portfolio/HireFreelancerCommandHandler.php
@@ -9,12 +9,17 @@ use App\Application\Portfolio\PortfolioRepository\PortfolioReadRepository;
 use App\Application\Portfolio\PortfolioRepository\PortfolioWriteRepository;
 use App\Domain\Portfolio\Portfolio;
 use Ramsey\Uuid\Uuid;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Message;
 
 final class HireFreelancerCommandHandler
 {
     public function __construct(
         public PortfolioReadRepository $portfolioReadRepository,
-        public PortfolioWriteRepository $portfolioWriteRepository
+        public PortfolioWriteRepository $portfolioWriteRepository,
+        public MailerInterface $mailer
     ) {
     }
 
@@ -25,11 +30,22 @@ final class HireFreelancerCommandHandler
         $portfolio->addHired($command->companyName);
 
         $this->portfolioWriteRepository->save($portfolio);
-        $this->sendMail($portfolio, $command->subject, $command->message);
+        $this->sendMail($portfolio, $command->subject, $command->message, $command->companyName);
     }
 
-    private function sendMail(?Portfolio $portfolio, string $subject, string $message)
+    private function sendMail(?Portfolio $portfolio, string $subject, string $message, string $companyName): void
     {
+        $email = (new TemplatedEmail())
+            ->from("info@liberato.io")
+            ->to(new Address($portfolio->getUser()->getEmail(), $portfolio->getUser()->getUsername()))
+            ->subject('Someone wants to hire you!')
+            ->htmlTemplate('emails/hire-freelancer.html.twig')
+            ->context([
+                'subject' => $subject,
+                'message' => $message,
+                'companyName' => $companyName,
+            ]);
 
+        $this->mailer->send($email);
     }
 }
