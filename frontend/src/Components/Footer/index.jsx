6c68763645e16821a3e95547c39e4bd00714fe86
Stipo Margic <stipo@liberato.io>
2021-07-10 15:49:17 +0200

    Simplify footer

--- frontend/src/Components/Footer/index.jsx
+++ frontend/src/Components/Footer/index.jsx
@@ -6,123 +6,58 @@ import {
   faInstagram,
   faLinkedin,
 } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 import logo from "../../Assets/images/logo.png";
 
 const Footer = () => {
   return (
     <div className="sticky-footer">
       <footer className="dark-footer skin-dark-footer">
-        <div>
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-4 col-md-6">
-                <div className="footer-widget">
-                  <img
-                    src={logo}
-                    className="img-fluid f-logo"
-                    width="120"
-                    alt=""
-                  />
-                  <p>
-                    Kopilica 5, 21000 Split
-                    <br />
-                    Croatia (local: Hrvatska)
-                  </p>
-                  <ul className="footer-bottom-social">
-                    <li>
-                      <a href="/">
-                        <FontAwesomeIcon icon={faFacebook} />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="/">
-                        <FontAwesomeIcon icon={faLinkedin} />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="/">
-                        <FontAwesomeIcon icon={faInstagram} />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-md-6">
+              <div className="footer-widget">
+                <img
+                  src={logo}
+                  className="img-fluid f-logo"
+                  width="120"
+                  alt=""
+                />
+                <p>
+                  Kopilica 5, 21000 Split
+                  <br />
+                  Croatia (local: Hrvatska)
+                </p>
+                <ul className="footer-bottom-social">
+                  <li>
+                    <a href="/">
+                      <FontAwesomeIcon icon={faFacebook} />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/">
+                      <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/">
+                      <FontAwesomeIcon icon={faInstagram} />
+                    </a>
+                  </li>
+                </ul>
               </div>
-              <div className="col-lg-2 col-md-4">
-                <div className="footer-widget">
-                  <h4 className="widget-title">Useful links</h4>
-                  <ul className="footer-menu">
-                    <li>
-                      <a href="/">About Us</a>
-                    </li>
-                    <li>
-                      <a href="/">FAQs Page</a>
-                    </li>
-                    <li>
-                      <a href="/">Checkout</a>
-                    </li>
-                    <li>
-                      <a href="/">Login</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-
-              <div className="col-lg-2 col-md-4">
-                <div className="footer-widget">
-                  <h4 className="widget-title">Developers</h4>
-                  <ul className="footer-menu">
-                    <li>
-                      <a href="/">Booking</a>
-                    </li>
-                    <li>
-                      <a href="/">Stays</a>
-                    </li>
-                    <li>
-                      <a href="/">Adventures</a>
-                    </li>
-                    <li>
-                      <a href="/">Author Detail</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-
-              <div className="col-lg-2 col-md-4">
-                <div className="footer-widget">
-                  <h4 className="widget-title">Useful links</h4>
-                  <ul className="footer-menu">
-                    <li>
-                      <a href="/">About Us</a>
-                    </li>
-                    <li>
-                      <a href="/">Jobs</a>
-                    </li>
-                    <li>
-                      <a href="/">Events</a>
-                    </li>
-                    <li>
-                      <a href="/">Press</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-
-              <div className="col-lg-2 col-md-4">
-                <div className="footer-widget">
-                  <h4 className="widget-title">Useful links</h4>
-                  <ul className="footer-menu">
-                    <li>
-                      <a href="/">Support</a>
-                    </li>
-                    <li>
-                      <a href="/">Contact Us</a>
-                    </li>
-                    <li>
-                      <a href="/">Privacy &amp; Terms</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+            </div>
+            <div className="col-lg-2 col-md-6 foot-link">
+              <Link to="/about">About us</Link>
+            </div>
+            <div className="col-lg-2 col-md-6 foot-link">
+              <Link to="/jobs">Jobs</Link>
+            </div>
+            <div className="col-lg-2 col-md-6 foot-link">
+              <Link to="/freelancers">Freelancers</Link>
+            </div>
+            <div className="col-lg-2 col-md-6 foot-link">
+              <Link to="/contact">Contact us</Link>
             </div>
           </div>
         </div>
