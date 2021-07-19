c526cb72f8ac14c59fb849b0cb86190a2b15b61a
Stipo Margic <stipo@liberato.io>
2021-07-19 18:05:11 +0200

    Edit user

--- backend/src/Infrastructure/UI/HTTP/Web/v1/Endpoint/User/UpdateUserController.php
+++ backend/src/Infrastructure/UI/HTTP/Web/v1/Endpoint/User/UpdateUserController.php
@@ -23,7 +23,7 @@ use Undabot\SymfonyJsonApi\Service\Resource\Factory\ResourceFactory;
 
 final class UpdateUserController
 {
-    #[Route('/api/v1/users/{id}', name: 'api_v1_users_update', methods: ['PUT, PATCh'])]
+    #[Route('/api/v1/users/{id}', name: 'api_v1_users_update', methods: ['PUT', 'PATCH'])]
     /** @IsGranted("ROLE_ADMIN") */
     public function update(
         UuidInterface $id,
