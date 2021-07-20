6cf44106c565541bf5844ddfb8d4f89302f3bded
Stipo Margic <stipo@liberato.io>
2021-07-20 12:21:16 +0200

    DCCH Updated

--- backend/src/Application/Service/CommandHandler/Web/Category/DeleteCategoryCommandHandler.php
+++ backend/src/Application/Service/CommandHandler/Web/Category/DeleteCategoryCommandHandler.php
@@ -8,6 +8,7 @@ namespace App\Application\Service\CommandHandler\Web\Category;
 use App\Application\Category\CategoryRepository\CategoryReadRepository;
 use App\Application\Category\CategoryRepository\CategoryWriteRepository;
 use App\Application\Command\Web\Category\DeleteCategoryCommand;
+use App\Application\Portfolio\PortfolioRepository\PortfolioWriteRepository;
 use App\Domain\Category\Exception\CategoryNotFoundException;
 use Ramsey\Uuid\Uuid;
 
@@ -15,7 +16,8 @@ class DeleteCategoryCommandHandler
 {
     public function __construct(
         private CategoryReadRepository $categoryReadRepository,
-        private CategoryWriteRepository $categoryWriteRepository
+        private CategoryWriteRepository $categoryWriteRepository,
+        private PortfolioWriteRepository $portfolioWriteRepository
     ) {
     }
 
@@ -26,6 +28,11 @@ class DeleteCategoryCommandHandler
 
         if ($category === null) {
             throw CategoryNotFoundException::withId($command->id);
+        }
+
+        foreach ($category->getPortfolios()->getValues() as $portfolio) {
+            $portfolio->delete();
+            $this->portfolioWriteRepository->save($portfolio);
         }
 
         $category->delete();
