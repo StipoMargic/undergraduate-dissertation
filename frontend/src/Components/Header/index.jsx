dbdc710321bcde3ada22149c882670df911c65ce
Stipo Margic <stipo@liberato.io>
2021-07-31 20:34:48 +0200

    Header is now responsive

--- frontend/src/Components/Header/index.jsx
+++ frontend/src/Components/Header/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import "./styles.scss";
 import React, {
   useCallback,
@@ -16,9 +17,10 @@ import {
 import axios from "axios";
 import Cookies from "js-cookie";
 import { useLocation } from "react-router";
-import logo from "../../Assets/images/logo.png";
 import { makeLoginData } from "./makeLoginData";
 import { GlobalContext } from "../../Context/global";
+import logo from "../../Assets/images/logo.png";
+import { ADMIN, COMPANY, FREELANCER } from "../../Constants/roles";
 
 const loginInitData = {
   username: "",
@@ -45,7 +47,7 @@ const Header = () => {
   const history = useHistory();
   const { setTokenWithCookie, role, username, removeAllCookies } =
     useContext(GlobalContext);
-  const [signIn, setSignIn] = useState(false);
+  const [, setSignIn] = useState(false);
   const [loginData, setLoginData] = useState(loginInitData);
   const [loginError, setLoginError] = useState(false);
   const ttl = Cookies.get("ttl");
@@ -72,10 +74,6 @@ const Header = () => {
     }
   }
 
-  const handleSignIn = (value) => {
-    setSignIn(value);
-  };
-
   const handleLogin = (e) => {
     e.preventDefault();
     axios
@@ -90,10 +88,6 @@ const Header = () => {
           res.data.username,
           res.data.role
         );
-
-        setSignIn(false);
-
-        history.push("/");
         window.location.reload();
       })
       .catch(() => setLoginError(true));
@@ -112,13 +106,12 @@ const Header = () => {
     return (
       <div ref={loginRef}>
         <div
-          className="modal-open"
-          id="login"
+          className="modal fade"
+          id="loginModal"
           tabIndex="-1"
           role="dialog"
-          aria-labelledby="registermodal"
+          aria-labelledby="exampleModalLabel"
           aria-hidden="true"
-          onFocusIn={() => handleSignIn(false)}
         >
           <div
             className="modal-dialog modal-dialog-centered login-pop-form"
@@ -129,46 +122,36 @@ const Header = () => {
                 <h4>Sign In</h4>
                 <button
                   type="button"
-                  onClick={() => handleSignIn(false)}
                   className="close"
+                  data-dismiss="modal"
                   aria-label="Close"
                 >
-                  <span aria-hidden="true">
-                    <FontAwesomeIcon
-                      icon={faWindowClose}
-                      size="2x"
-                      color="red"
-                    />
-                  </span>
+                  <FontAwesomeIcon icon={faWindowClose} color="red" />
                 </button>
               </div>
               <div className="modal-body">
                 <div className="login-form">
                   <form onSubmit={handleLogin}>
                     <div className="form-group">
-                      <label htmlFor="username">
-                        User Name
-                        <input
-                          id="username"
-                          onChange={onInputChange("username")}
-                          type="text"
-                          className="form-control"
-                          placeholder="Username"
-                        />
-                      </label>
+                      <label htmlFor="username">User Name</label>
+                      <input
+                        id="username"
+                        onChange={onInputChange("username")}
+                        type="text"
+                        className="w-100 form-control"
+                        placeholder="Username"
+                      />
                     </div>
 
                     <div className="form-group">
-                      <label htmlFor="password">
-                        Password
-                        <input
-                          onChange={onInputChange("password")}
-                          id="password"
-                          type="password"
-                          className="form-control"
-                          placeholder="*******"
-                        />
-                      </label>
+                      <label htmlFor="password">Password</label>
+                      <input
+                        onChange={onInputChange("password")}
+                        id="password"
+                        type="password"
+                        className="w-100 form-control"
+                        placeholder="*******"
+                      />
                     </div>
 
                     <div className="form-group">
@@ -190,8 +173,8 @@ const Header = () => {
                       Something went wrong! Try again...
                     </p>
                   )}
-                  <FontAwesomeIcon icon={faUser} /> Have not An Account?
-                  <Link to="/register" className="theme-cl">
+                  <FontAwesomeIcon icon={faUser} /> Not a member yet?
+                  <Link to="/register" className="ml-1">
                     Sign Up
                   </Link>
                 </div>
@@ -202,116 +185,135 @@ const Header = () => {
       </div>
     );
   };
+
   return (
     <>
-      {signIn ? renderSignInModal() : ""}
-      <div className="header header-transparent dark-text">
+      <nav
+        className="navbar navbar-expand-lg navbar-light"
+        style={{ backgroundColor: "#F4F9FD" }}
+      >
         <div className="container">
-          <div className="row">
-            <div className="col-lg-12 col-md-12 col-sm-12">
-              <nav
-                id="navigation"
-                className="mt-2 navigation navigation-landscape shadow-sm"
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a className="navbar-brand" href="/">
+              <img src={logo} style={{ width: "150px" }} alt="" />
+            </a>
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+              <li className={pathname === "/" ? "active nav-item" : "nav-item"}>
+                <Link className="nav-link" to="/">
+                  Home{" "}
+                </Link>
+              </li>
+
+              {role !== FREELANCER && (
+                <li
+                  className={
+                    pathname === "/freelancers"
+                      ? "acti1ve nav-item"
+                      : "nav-item"
+                  }
+                >
+                  <Link className="nav-link" to="/freelancers">
+                    Freelancers
+                  </Link>
+                </li>
+              )}
+
+              {role !== COMPANY && (
+                <li
+                  className={
+                    pathname === "/jobs" ? "active nav-item" : "nav-item"
+                  }
+                >
+                  <Link className="nav-link" to="/jobs">
+                    Jobs
+                  </Link>
+                </li>
+              )}
+
+              <li
+                className={
+                  pathname === "/about" ? "active nav-item" : "nav-item"
+                }
               >
-                <div className="nav-header">
-                  <a className="nav-brand" href="/">
-                    <img src={logo} className="logo" alt="" />
-                  </a>
-                </div>
-                <div className="nav-menus-wrapper">
-                  <ul className="nav-menu">
-                    <li className={pathname === "/" ? "active" : ""}>
-                      <Link to="/">Home </Link>
-                    </li>
-
-                    {role !== "ROLE_USER" && (
-                      <li
-                        className={pathname === "/freelancers" ? "active" : ""}
-                      >
-                        <Link to="/freelancers">Freelancers</Link>
-                      </li>
-                    )}
-
-                    {role !== "ROLE_EMPLOYER" && (
-                      <li className={pathname === "/jobs" ? "active" : ""}>
-                        <Link to="/jobs">Jobs</Link>
-                      </li>
-                    )}
-
-                    <li className={pathname === "/about" ? "active" : ""}>
-                      <Link to="/about">About Us</Link>
-                    </li>
-                    <li className={pathname === "/contact" ? "active" : ""}>
-                      <Link to="/contact">Contact Us</Link>
-                    </li>
-                  </ul>
-
-                  {!username ? (
-                    <ul className="nav-menu align-to-right">
-                      <li>
-                        <button
-                          type="button"
-                          className="btn btn-outline-primary text-dark mr-2 mt-4"
-                          onClick={() => handleSignIn(!signIn)}
-                        >
-                          <FontAwesomeIcon icon={faUser} /> Sign in
-                        </button>
-                      </li>
-                      <li>
-                        <Link to="/register">
-                          <button
-                            className="btn btn-primary text-light mr-4 mtminus"
-                            type="button"
-                          >
-                            <FontAwesomeIcon icon={faLock} /> Register
-                          </button>
-                        </Link>
-                      </li>
-                    </ul>
-                  ) : (
-                    <ul className="nav-menu align-to-right">
-                      <li>
-                        <Link to={`/profile/${username}`}>
-                          <FontAwesomeIcon icon={faUser} className="mr-2" />
-                          Hi {username}
-                        </Link>
-                        <Link to="/logout">
-                          <button
-                            type="button"
-                            className="btn btn-outline-primary text-dark mr-1"
-                          >
-                            Logout
-                          </button>
-                        </Link>
-                      </li>
-                      {role === "ROLE_ADMIN" ? (
-                        <li>
-                          <Link to="/admin">
-                            <button type="button" className="btn btn-danger">
-                              Admin
-                            </button>
-                          </Link>
-                        </li>
-                      ) : role === "ROLE_EMPLOYER" || role === "ROLE_USER" ? (
-                        <li>
-                          <Link to="/add-job">
-                            <button className="btn btn-primary" type="button">
-                              Add job ad
-                            </button>
-                          </Link>
-                        </li>
-                      ) : (
-                        ""
-                      )}
-                    </ul>
-                  )}
-                </div>
-              </nav>
-            </div>
+                <Link className="nav-link" to="/about">
+                  About Us
+                </Link>
+              </li>
+              <li
+                className={
+                  pathname === "/contact" ? "active nav-item" : "nav-item"
+                }
+              >
+                <Link className="nav-link" to="/contact">
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+            {username ? (
+              <>
+                <Link className="mr-2" to={`/profile/${username}`}>
+                  <FontAwesomeIcon icon={faUser} className="mr-2" />
+                  Hi {username}
+                </Link>
+                <Link to="/logout">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary text-dark mr-1"
+                  >
+                    Logout
+                  </button>
+                </Link>
+              </>
+            ) : (
+              <>
+                <button
+                  type="button"
+                  data-toggle="modal"
+                  data-target="#loginModal"
+                  className="btn btn-outline-primary mr-1 my-2 my-sm-0"
+                >
+                  <FontAwesomeIcon icon={faUser} /> Sign in
+                </button>
+                <Link to="/register">
+                  <button
+                    className="btn btn-outline-success mr-1 my-2 my-sm-0"
+                    type="button"
+                  >
+                    <FontAwesomeIcon icon={faLock} /> Register
+                  </button>
+                </Link>
+              </>
+            )}
+            {role === ADMIN ? (
+              <Link to="/admin">
+                <button type="button" className="btn btn-danger">
+                  Admin
+                </button>
+              </Link>
+            ) : role === COMPANY || role === FREELANCER ? (
+              <Link to="/add-job">
+                <button className="btn btn-primary" type="button">
+                  Add job ad
+                </button>
+              </Link>
+            ) : (
+              ""
+            )}
           </div>
         </div>
-      </div>
-      <div className="clearfix" />
+      </nav>
+      {renderSignInModal()}
     </>
   );
 };
