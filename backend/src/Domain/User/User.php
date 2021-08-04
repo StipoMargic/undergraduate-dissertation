15845731bbd5b8ef1d405a417d61f7b1f492b0c3
Stipo Margic <stipo@liberato.io>
2021-08-04 17:54:40 +0200

    User update

--- backend/src/Domain/User/User.php
+++ backend/src/Domain/User/User.php
@@ -99,7 +99,7 @@ class User implements UserInterface, EntityInterface
     /** @ORM\Column(name="linkedin", type="string", nullable=true) */
     private ?string $linkedin;
 
-    /** @ORM\Column(name="about", type="string", nullable=false, length=500) */
+    /** @ORM\Column(name="about", type="text", nullable=false) */
     private string $about;
 
     /**
