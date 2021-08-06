2c306cdcb7529ea6037ca1dcbb3c0a7fe68e9833
Stipo Margic <stipo@liberato.io>
2021-08-06 20:01:51 +0200

    Approved/Declined email

--- backend/src/Application/Service/CommandHandler/Web/Job/JobApproveCommandHandler.php
+++ backend/src/Application/Service/CommandHandler/Web/Job/JobApproveCommandHandler.php
@@ -7,14 +7,19 @@ namespace App\Application\Service\CommandHandler\Web\Job;
 use App\Application\Command\Web\Job\JobApproveCommand;
 use App\Application\Job\JobRepository\JobReadRepository;
 use App\Application\Job\JobRepository\JobWriteRepository;
+use App\Application\User\UserRepository\UserReadRepository;
+use App\Domain\User\User;
 use Ramsey\Uuid\Uuid;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 
 class JobApproveCommandHandler
 {
     public function __construct(
         private JobReadRepository $jobReadRepository,
         private JobWriteRepository $jobWriteRepository,
+        private UserReadRepository $userReadRepository,
         public MailerInterface $mailer
     ) {
     }
@@ -22,8 +27,25 @@ class JobApproveCommandHandler
     public function __invoke(JobApproveCommand $command): void
     {
         $job = $this->jobReadRepository->get(Uuid::fromString($command->jobId));
-
+        $applicant = $this->userReadRepository->getByUsername($command->applicantName);
+        $companyName = $job->getUser()->getUsername();
+        $this->sendEmail($companyName, $command->applicantName, $applicant->getEmail());
         $job->approveApplication($command->applicantName);
         $this->jobWriteRepository->save($job);
+    }
+
+    private function sendEmail(string $companyName, string $applicantName, string $applicantEmail): void
+    {
+        $email = (new TemplatedEmail())
+            ->from("info@liberato.io")
+            ->to(new Address($applicantEmail, $applicantName))
+            ->subject('You got approved!')
+            ->htmlTemplate('emails/approved.html.twig')
+            ->context([
+                'companyName' => $companyName,
+                'applicantName' => $applicantName,
+            ]);
+
+        $this->mailer->send($email);
     }
 }
--- backend/src/Application/Service/CommandHandler/Web/Job/JobDeclineCommandHandler.php
+++ backend/src/Application/Service/CommandHandler/Web/Job/JobDeclineCommandHandler.php
@@ -7,14 +7,18 @@ namespace App\Application\Service\CommandHandler\Web\Job;
 use App\Application\Command\Web\Job\JobDeclineCommand;
 use App\Application\Job\JobRepository\JobReadRepository;
 use App\Application\Job\JobRepository\JobWriteRepository;
+use App\Application\User\UserRepository\UserReadRepository;
 use Ramsey\Uuid\Uuid;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 
 class JobDeclineCommandHandler
 {
     public function __construct(
         private JobReadRepository $jobReadRepository,
         private JobWriteRepository $jobWriteRepository,
+        private UserReadRepository $userReadRepository,
         public MailerInterface $mailer
     ) {
     }
@@ -22,8 +26,25 @@ class JobDeclineCommandHandler
     public function __invoke(JobDeclineCommand $command): void
     {
         $job = $this->jobReadRepository->get(Uuid::fromString($command->jobId));
-
+        $applicant = $this->userReadRepository->getByUsername($command->applicantName);
+        $companyName = $job->getUser()->getUsername();
+        $this->sendEmail($companyName, $command->applicantName, $applicant->getEmail());
         $job->declineApplication($command->applicantName);
         $this->jobWriteRepository->save($job);
+    }
+
+    private function sendEmail(string $companyName, string $applicantName, string $applicantEmail): void
+    {
+        $email = (new TemplatedEmail())
+            ->from("info@liberato.io")
+            ->to(new Address($applicantEmail, $applicantName))
+            ->subject('You got declined!')
+            ->htmlTemplate('emails/declined.html.twig')
+            ->context([
+                'companyName' => $companyName,
+                'applicantName' => $applicantName,
+            ]);
+
+        $this->mailer->send($email);
     }
 }
